c49efc85a8b65384bdc91c009e624385c451122d
Robin C. <robin.combeau@gmail.com>
2023-10-18 18:16:33 +0200

    feat new initial grid

--- src/components/sudoku/Grid.jsx
+++ src/components/sudoku/Grid.jsx
@@ -1,20 +1,30 @@
 import React, { useState, useEffect } from 'react';
 
 export default function Grid({ buttonClickInfo, setButtonClickInfo }) {
-  //TODO - mettre une grille aléatoire au début
-  const initialGrid = [
-    [0, 0, 4, 0, 5, 0, 0, 0, 0],
-    [9, 0, 0, 7, 3, 4, 6, 0, 0],
-    [0, 0, 3, 0, 2, 1, 0, 4, 9],
-    [0, 3, 5, 0, 9, 0, 4, 8, 0],
-    [0, 9, 0, 0, 0, 0, 0, 3, 0],
-    [0, 7, 6, 0, 1, 0, 9, 2, 0],
-    [3, 1, 0, 9, 7, 0, 2, 0, 0],
-    [0, 0, 9, 1, 8, 2, 0, 0, 3],
-    [0, 0, 0, 0, 6, 0, 1, 0, 0]
-  ];
+  const [grid, setGrid] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+  ]);
 
-  const [grid, setGrid] = useState(initialGrid);
+  useEffect(() => {
+    // Fetch the initial Sudoku grid from the API when the component mounts
+    fetch('https://sudoku-api.vercel.app/api/dosuku')
+      .then((response) => response.json())
+      .then((data) => {
+        const newGrid = data.newboard.grids[0].value;
+        setGrid(newGrid);
+      })
+      .catch((error) => {
+        console.error('Error fetching initial grid:', error);
+      });
+  }, []);
 
   // Update grid on every right input
   const inputChange = (event, rowIndex, colIndex) => {
@@ -49,11 +59,12 @@ export default function Grid({ buttonClickInfo, setButtonClickInfo }) {
       clearGrid();
       setButtonClickInfo('');
     }
-  }, [buttonClickInfo, setButtonClickInfo]);
+  }, [buttonClickInfo]);
 
   // "New" Button
   useEffect(() => {
     if (buttonClickInfo === 'new') {
+      setGrid(null);
       // Fetch a new grid
       fetch('https://sudoku-api.vercel.app/api/dosuku')
         .then((response) => response.json())
@@ -66,7 +77,7 @@ export default function Grid({ buttonClickInfo, setButtonClickInfo }) {
         });
       setButtonClickInfo('');
     }
-  }, [buttonClickInfo, setButtonClickInfo, setGrid]);
+  }, [buttonClickInfo]);
 
 
   // Solve Sudoku algorithm
@@ -130,7 +141,11 @@ export default function Grid({ buttonClickInfo, setButtonClickInfo }) {
       solveSudoku();
       setButtonClickInfo('');
     }
-  }, [buttonClickInfo, setButtonClickInfo]);
+  }, [buttonClickInfo]);
+
+  if (grid === null) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <>
@@ -153,7 +168,7 @@ export default function Grid({ buttonClickInfo, setButtonClickInfo }) {
               type="number"
               value={value || ''} // if value = false (0 is false)
               onChange={(e) => inputChange(e, rowIndex, colIndex)}
-              className="w-8 h-8 text-center border border-slate-300 focus:outline-0"
+              className="w-8 h-8 text-center border border-slate-300 focus:outline-0 focus:border-slate-500"
               data-x={colIndex}
               data-y={rowIndex}
             />
